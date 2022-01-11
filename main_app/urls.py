a71b1f73a7633a8163425bcb4075b76e9b53156c
Chris Kehoe <chriskehoe@Chriss-MacBook-Air.local>
2022-01-11 12:39:57 -0800

    built add star page and button

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about),
-    path('stars/', views.stars_index, name='index')
+    path('about/', views.about, name='about'),
+    path('stars/', views.stars_index, name='index'),
+    path('stars/<int:star_id>/', views.stars_detail, name='detail'),
+    path('stars/create/', views.StarCreate.as_view(), name='stars_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
+from django.db.models import fields
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+
+from .models import Star
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -8,4 +11,16 @@ def about(request):
     return render(request, 'about.html')
 
 def stars_index(request):
+    stars = Star.objects.all()
     return render(request, 'stars/index.html', { 'stars' : stars})
+
+def stars_detail(request, star_id):
+    star = Star.objects.get(id=star_id)
+    return render(request, 'stars/detail.htlm', {'star' : star})
+
+
+#class based objects below
+
+class StarCreate(CreateView):
+    model = Star
+    fields = '__all__'
