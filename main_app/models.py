fa2f5664ca79f2a44193ec8bc2abb4df236936da
Chris Kehoe <chriskehoe@Chriss-MacBook-Air.local>
2022-01-12 16:06:30 -0800

    built out edit button that redirects to edit page, need to make feilds appear

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
+WEAPONRY = (
+    ('S', 'Sword'),
+    ('G', 'Gun'),
+    ('H', 'Hand-to-Hand')
+)
+
 class Star(models.Model):  # Note that parens are optional if not inheriting from another class
     name = models.CharField(max_length = 100) 
     powerlvl = models.IntegerField()
@@ -14,3 +20,13 @@ class Star(models.Model):  # Note that parens are optional if not inheriting fro
     def get_absolute_url(self):
         return reverse('detail', kwargs={'star_id' : self.id})
 
+class Weapon(models.Model):
+    weaponry = models.CharField(
+        max_length=1,
+        choices=WEAPONRY,
+        default=WEAPONRY[0][0])
+    
+    star = models.ForeignKey(Star, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_weaponry_display()}"
